620dd77b47333b7f1a1b8a632bb01f6a680e1911
Chris Smith <chris@chameth.com>
2021-07-04 02:55:10 +0100

    Initial version

---
+++ config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config provides an easy way to read and write configuration files for desktop applications.
+//
+// Config files are stored in the appropriate location (per os.UserConfigDir()), and automatically marshalled to and
+// from YAML.
+type Config struct {
+	*options
+	path string
+}
+
+// New creates a new Config that can be used to load and save configuration information.
+// Options can be passed to customise behaviour, but the defaults are designed to be usable out of the box.
+func New(o ...Option) (*Config, error) {
+	opts := newOptions(o)
+
+	p, err := opts.path()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		options: opts,
+		path:    p,
+	}, nil
+}
+
+// Load reads the config file from disk, if it exists, and unmarshals it into the target struct.
+// If the file does not exist on disk, no changes will be made to the target but no error will be returned.
+func (c *Config) Load(target interface{}) error {
+	b, err := os.ReadFile(c.path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	return yaml.Unmarshal(b, target)
+}
+
+// Save marshals and writes the config file to disk. If the config directory does not exist it will be created.
+func (c *Config) Save(i interface{}) error {
+	b, err := yaml.Marshal(i)
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(filepath.Dir(c.path), os.FileMode(0700)); err != nil {
+		return err
+	}
+
+	return os.WriteFile(c.path, b, os.FileMode(0600))
+}
---
+++ options.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// An Option alters the default configuration of the config provider.
+type Option func(*options)
+
+// DirectoryName changes the name of the directory that the config file should be stored in.
+//
+// This is usually the application's name, and if not specified will default to os.Args[0].
+func DirectoryName(n string) Option {
+	return func(o *options) {
+		o.directory = n
+	}
+}
+
+// FileName changes the name of the file that the config is stored in.
+//
+// If not specified, defaults to "config.yml".
+func FileName(n string) Option {
+	return func(o *options) {
+		o.filename = n
+	}
+}
+
+// Permissions sets the filesystem permissions that will be set on newly created directories and files.
+// Existing permissions will not be modified.
+//
+// If not specified, defaults to 0700 for directories and 0600 for files.
+func Permissions(directoryMode os.FileMode, fileMode os.FileMode) Option {
+	return func(o *options) {
+		o.directoryMode = directoryMode
+		o.fileMode = fileMode
+	}
+}
+
+type options struct {
+	directory     string
+	directoryMode os.FileMode
+
+	filename string
+	fileMode os.FileMode
+}
+
+func (o *options) path() (string, error) {
+	base, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(base, o.directory, o.filename), nil
+}
+
+func newOptions(o []Option) *options {
+	c := &options{
+		filename:      "config.yml",
+		fileMode:      os.FileMode(0600),
+		directory:     os.Args[0],
+		directoryMode: os.FileMode(0700),
+	}
+
+	for i := range o {
+		o[i](c)
+	}
+
+	return c
+}
